3c34d137ae856d47e5e55798ede556aa86d8f3c0
LakshmiMounika19 <32009660+LakshmiMounika19@users.noreply.github.com>
2017-09-16 14:36:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.utill.*;
+public class mouni 
+{
+public static void main(String[] args) 
+{
+	Student st=new Student();
+        st.Student();
+    	st.getId(); 
+        st.setId(25);
+        st.getFullName();
+        st.setFullName("Sagar");
+        st.getBirthDate();
+        st.setBirthDate("1997-03-18");
+        st.getAvgMark();
+        st.setAvgMark(98.87);
+
+
+}
+}
